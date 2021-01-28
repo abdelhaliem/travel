4994d1db23ad04bed6a3b6c8fd12a209d25835ef
AEhamza <73121329+abdelhaliem@users.noreply.github.com>
2021-01-28 23:47:34 +0200

    Add files via upload

---
+++ bin/teavel_agency.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+void main() {
+
+
+
+  print("Enter Name");
+
+  var Trip = "222";
+
+  print(Trip);
+
+
+}
+
+
+////}
+//Set GizaPyramids(){
+//  int id ;
+//  String location ;
+//  double price ;
+//  String  date ;
+//  double number_of_travelers ;
+//
+//}
+
+
+
+
+//List <Map<String,dynamic>> Trips = [
+//{'id': 1 ,'location':'egypt','price':10000,'date':'2020/11/2','number_of_travelers' :11000 },
+//{'id': 2 ,'location':'egypt','price':10000,'date':'2020/11/2','number_of_travelers' :11000 }
+//,{'id': 3 ,'location':'egypt','price':10000,'date':'2020/11/2','number_of_travelers' :11000 }
+
+//];
+//
+//Trips({}) {
+//  var x = id,
+//      location,
+//      price,
+//      date,
+//      number_of_travelers;
+//  print(x);
+//}
